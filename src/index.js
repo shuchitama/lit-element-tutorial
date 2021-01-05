b05b478608d85741fc35a70b7508a6b346fbcf53
Shuchita Mahey <shuchita@live.com>
2021-01-04 16:18:47 -0800

    set up routing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import "./styles.css";
-
 import "./views/todo-view";
+import { Router } from "@vaadin/router";
+
+window.addEventListener("load", () => {
+	initRouter();
+});
+
+function initRouter() {
+	const router = new Router(document.querySelector("main"));
+	router.setRoutes([
+		{
+			path: "/",
+			component: "todo-view",
+		},
+		{
+			path: "/stats",
+			component: "stats-view",
+			action: () => import(".views/stats-view"),
+		},
+		{
+			path: "/*",
+			component: "not-found-view",
+			action: () => import(".views/not-found-view"),
+		},
+	]);
+}
